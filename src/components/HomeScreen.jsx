cc385bd6b85017e41d0440f171e5b3fdc29d4906
Jordan <jordancdiaz04@gmail.com>
2023-10-25 23:36:39 -0400

    design landing page

---
+++ src/components/HomeScreen.jsx
@@ -0,0 +1,16 @@
+import Gallery from "../assets/gallery.png";
+
+export default function HomeScreen() {
+  return (
+    <>
+      <div className="HomeScreen">
+        <h1>TERMINA IS UPON US</h1>
+        <h2>
+          Use the <span>Contestant Creator</span> to create a new contestant, so
+          that the festival may begin...
+        </h2>
+        <img src={Gallery} id="contestants" />
+      </div>
+    </>
+  );
+}
