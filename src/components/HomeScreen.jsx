93c0b0ad7a7afd4148dac329fe7722d23559fc6b
Jordan <jordancdiaz04@gmail.com>
2023-10-28 18:20:39 -0400

    finalize landing page

--- src/components/HomeScreen.jsx
+++ src/components/HomeScreen.jsx
@@ -4,12 +4,14 @@ export default function HomeScreen() {
   return (
     <>
       <div className="HomeScreen">
-        <h1>TERMINA IS UPON US</h1>
-        <h2>
-          Use the <span>Contestant Creator</span> to create a new contestant, so
-          that the festival may begin...
-        </h2>
-        <img src={Gallery} id="contestants" />
+        <div className="HomeScreen-body">
+          <h1>TERMINA IS UPON US</h1>
+          <h2>
+            Use the <span>Contestant Creator</span> to create a new contestant,
+            so that the festival may begin...
+          </h2>
+          <img src={Gallery} id="contestants" />
+        </div>
       </div>
     </>
   );
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import "../App.css";
+import HomeScreen from "../components/HomeScreen";
+
+export default function Home() {
+  return (
+    <>
+      <div className="App">
+        <HomeScreen />
+      </div>
+    </>
+  );
+}
