9c0e9ea148398ae530d421ec20624be439b7aba7
Jordan <jordancdiaz04@gmail.com>
2023-10-29 23:37:50 -0400

    finalize app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,55 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import Sidebar from "./components/Sidebar";
+import Home from "./pages/Home";
+import Creation from "./pages/Creation";
+import ContestantGallery from "./pages/ContestantGallery";
+import ContestantDetails from "./pages/ContestantDetails";
+import supabase from "./supabase";
 import "./App.css";
-import Karin from "./assets/Karin_Portrait_full.png";
-import Marcoh from "./assets/Marcoh.png";
-import Marina from "./assets/Marina.png";
 
 function App() {
-  console.log(`APIKEY: ${import.meta.env.VITE_APIKEY}`);
+  const [databaseContestants, setDatabaseContestants] = useState([]);
+
+  const fetchContestants = async () => {
+    const { data } = await supabase
+      .from("Contestants")
+      .select()
+      .order("created_at", { ascending: false });
+    setDatabaseContestants(data);
+  };
+
+  //create a useEffect that fetches the databaseContestants when the :id route is loaded
+
   return (
     <>
-      <h1>APP</h1>
-      <img src={Karin} alt="" />
-      <img src={Marina} alt="" />
-      <img src={Marcoh} alt="" />
+      <BrowserRouter>
+        <div className="App">
+          <Sidebar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/creation" element={<Creation />} />
+            <Route
+              path="/contestants"
+              element={
+                <ContestantGallery
+                  databaseContestants={databaseContestants}
+                  setDatabaseContestants={setDatabaseContestants}
+                  fetchContestants={fetchContestants}
+                />
+              }
+            />
+            <Route
+              path="/contestants/:id"
+              element={<ContestantDetails editMode={false} />}
+            />
+            <Route
+              path="/contestants/edit/:id"
+              element={<ContestantDetails editMode={true} />}
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
   );
 }
